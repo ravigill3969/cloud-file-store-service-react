36194e73974442b73309a4cdd71f8135fc0a91c2
ravigill3936 <gill1234ravi@gmail.com>
2025-07-08 05:57:08 -0400

    installed shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <div className="text-blue-500">water</div>
+      <Button >waer</Button>
     </>
   );
 }
